578e2655fff76fed52777281f550932a20f3bf11
De Zhang Lee <31544865+dezhanglee@users.noreply.github.com>
2018-04-27 18:15:26 +0800

    Add files via upload

---
+++ codecrunch_to_ivle.py
@@ -0,0 +1,57 @@
+import csv
+import math
+
+dct = {}
+
+'''
+accepts csv files containing marks as input, combines and scales the marks,
+stores in dct (above)
+'''
+def getMarks(*filenames):
+
+    for i in filenames:
+        with open(i) as csvfile:
+            file_reader = csv.reader(csvfile)
+            for row in file_reader:
+                try:
+                    if (row[1] not in dct):
+                        dct[row[1]] = [float(row[-3])]
+                    else:
+                        dct[row[1]].append(float(row[-3]))
+                #to handle the header
+                except:
+                    continue 
+    
+    for i in dct:
+        dct[i].sort()
+        if len(dct[i]) == 3:
+            bestTwo = sum(dct[i][1:])
+        elif len(dct[i]) == 2:
+            bestTwo = sum(dct[i])
+        elif len(dct[i]) == 1:
+            bestTwo = dct[i][0]
+        else:
+            bestTwo = 0
+        dct[i] = math.ceil(bestTwo*100)/200 #round to 2 decimal places. 
+
+'''
+Uses data stored in dct to write to csv
+'''
+def write_to_file():
+
+    with open("for_ivle_upload.csv", "w", newline='') as csvfile:
+
+        filewriter = csv.writer(csvfile, delimiter=',',quotechar='|', \
+                                quoting=csv.QUOTE_MINIMAL)
+        
+        filewriter.writerow(["USER_ID","MARKS","REMARKS",\
+                             "GRADES","MODERATIONS"])
+
+        for student in dct:
+
+            marks = dct[student]
+            
+            entry = [str(student), marks, "Please refer to Codecrunch. Average of best 2 scores taken. "]
+
+            filewriter.writerow(entry)
+
