7f25a5a6cf293fc5cb99f0125a5f23de20625cfe
De Zhang Lee <31544865+dezhanglee@users.noreply.github.com>
2018-04-27 18:22:53 +0800

    Update codecrunch_to_ivle.py

--- codecrunch_to_ivle.py
+++ codecrunch_to_ivle.py
@@ -23,13 +23,11 @@ def getMarks(*filenames):
                     continue 
     
     for i in dct:
-        dct[i].sort()
         if len(dct[i]) == 3:
+            dct.sort() #sort in ascending order, select last 2 as the best 2 
             bestTwo = sum(dct[i][1:])
-        elif len(dct[i]) == 2:
+        elif len(dct[i]) in (1, 2): #if student submitted 1/2 entries, then just select them.
             bestTwo = sum(dct[i])
-        elif len(dct[i]) == 1:
-            bestTwo = dct[i][0]
         else:
             bestTwo = 0
         dct[i] = math.ceil(bestTwo*100)/200 #round to 2 decimal places. 
